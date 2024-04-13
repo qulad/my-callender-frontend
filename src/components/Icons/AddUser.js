9bef0194041e8544db737d03a710ce26261e6b11
Ceyhun-gl <ceyhungul148@gmail.com>
2024-04-13 23:11:02 +0300

    user page complated

---
+++ src/components/Icons/AddUser.js
@@ -0,0 +1,17 @@
+export const AddUser = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 14 14"
+    {...props}
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M5 5.5a2.5 2.5 0 1 0 0-5a2.5 2.5 0 0 0 0 5m1.5 7h-6v-.542a4.51 4.51 0 0 1 6.5-4m3.5-.458v6m-3-3h6"
+    ></path>
+  </svg>
+);
--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,7 +1,84 @@
+import { AddUser } from "components/Icons/AddUser";
+import { Dots } from "components/Icons/Dots";
+import { Verify } from "components/Icons/Verify";
+import PrivateLayout from "layouts/PrivateLayout";
+import EventCardLarge from "pages/home/components/EventCardLarge";
 import React from "react";
 
 const User = () => {
-  return <div>User</div>;
+  let profileAddress =
+    "https://t3.ftcdn.net/jpg/02/43/12/34/360_F_243123463_zTooub557xEWABDLk0jJklDyLSGl2jrr.jpg";
+  const mainEventList = [
+    {
+      eventName: "Yaratıcı Yazma Atölyesi",
+      location: "Şehir Kütüphanesi, Salon A",
+      date: "25 Nisan 2024",
+      id: "ETNKL1234",
+      address: "Beylikdüzü, İstanbul",
+      desc: "Kendinizi ifade etmenin yaratıcı yollarını keşfedin! Kendinizi ifade etmenin yaratıcı yollarını keşfedin! Kendinizi ifade etmenin yaratıcı yollarını keşfedin! Kendinizi ifade etmenin yaratıcı yollarını keşfedin!Kendinizi ifade etmenin yaratıcı yollarını keşfedin! Kendinizi ifade etmenin yaratıcı yollarını keşfedin!",
+      userName: "Ertuğrul Ahmet",
+      userMail: "test@gmail.com",
+      img: "https://t3.ftcdn.net/jpg/02/43/12/34/360_F_243123463_zTooub557xEWABDLk0jJklDyLSGl2jrr.jpg",
+    },
+  ];
+  return (
+    <PrivateLayout>
+      <div className="flex  justify-center  items-center gap-x-20 my-10 ">
+        <img
+          src={profileAddress}
+          alt="ProfilePhoto"
+          className="w-44 h-44 rounded-full"
+        />
+        <div className="flex flex-col">
+          <div className="flex gap-x-5 items-center">
+            <div className="flex gap-x-2 items-center">
+              ertuğrulAhmet <Verify />
+            </div>
+            <button className="p-1 rounded-md text-sm bg-gray-200 text-black">
+              Arkadaş Ekle
+            </button>
+            <button className="p-1 rounded-md text-sm bg-gray-200 text-black">
+              Mesaj gönder
+            </button>
+            <div className="flex justify-center items-center bg-gray-200 p-1 rounded-md">
+              <AddUser />
+            </div>
+            <Dots />
+          </div>
+          <div className="flex gap-x-10 font-semibold text-sm mt-5">
+            <p>
+              1.156 <span className="font-normal">gönderi</span>
+            </p>
+            <p>
+              7.6 M <span className="font-normal">takipçi</span>
+            </p>
+            <p>
+              362 <span className="font-normal">takip</span>
+            </p>
+          </div>
+          <p className="font-semibold mt-5">Ahmet Ertuğrul</p>
+        </div>
+      </div>
+      <hr className="w-full h-1"></hr>
+      <div className="flex flex-col  justify-center items-center mt-5 ">
+        <h1 className="text-gray-500 text-xl my-4">Etkinlikler</h1>
+        <div className="w-[800px] shadow p-4 rounded-md">
+          {mainEventList.map((allEvent) => (
+            <EventCardLarge
+              eventName={allEvent.eventName}
+              location={allEvent.location}
+              desc={allEvent.desc}
+              key={allEvent.id}
+              userMail={allEvent.userMail}
+              userName={allEvent.userName}
+              img={allEvent.img}
+              address={allEvent.address}
+            />
+          ))}
+        </div>
+      </div>
+    </PrivateLayout>
+  );
 };
 
 export default User;
